f90c9ce7b277bdb772cc588900a584628246c74a
Pankaj Singh <psingh2@univoedtech.com>
2024-05-07 16:08:32 +0530

    added hotel detail page and e2e test cases

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -29,3 +29,14 @@ test("Should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Pankaj")).toBeVisible();
   await expect(page.getByText("Pankaj Singh")).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("Pankaj");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Pankaj Singh Rawat").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
